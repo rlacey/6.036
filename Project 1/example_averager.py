b82ca5d479a675974618543f0180087f8ba84292
Ryan Lacey <rlacey@Ryans-MacBook-Air.local>
2014-02-12 17:06:36 -0500

    Project 1 - Simplified k-fold cross validation matrix splicing.

--- Project 1/example_averager.py
+++ Project 1/example_averager.py
@@ -1,9 +1,9 @@
 import numpy as np
 import project1_code as p1
-    
-dictionary = p1.extract_dictionary('M:/School/6.036/Project 1/train-tweet.txt')
-labels = p1.read_vector_file('M:/School/6.036/Project 1/train-answer.txt')
-feature_matrix = p1.extract_feature_vectors('M:/School/6.036/Project 1/train-tweet.txt', dictionary)
+
+dictionary = p1.extract_dictionary('train-tweet.txt')
+labels = p1.read_vector_file('train-answer.txt')
+feature_matrix = p1.extract_feature_vectors('train-tweet.txt', dictionary)
 
 average_theta = p1.averager(feature_matrix, labels)
 label_output = p1.perceptron_classify(feature_matrix, 0, average_theta)
@@ -26,6 +26,6 @@ for i in xrange(0, len(label_output)):
 
 percentage_correct = 100.0 * correct / len(label_output)
 print("Perceptron gets " + str(percentage_correct) + "% correct (" + str(correct) + " out of " + str(len(label_output)) + ").")
-                
-perceptron_performance = p1.cross_validation(feature_matrix, labels, 5)
+
+perceptron_performance = p1.cross_validation(feature_matrix, labels, 3)
 print("Perceptron gets " + str(perceptron_performance) + "% correct on average via cross-validation.")
